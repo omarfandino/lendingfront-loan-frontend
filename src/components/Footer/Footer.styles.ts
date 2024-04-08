53c45f481f4a534fd602964ec3f73eeb512f3bfb
Omar Fandiño <fandino.omar@gmail.com>
2024-04-07 20:14:46 -0500

    MarginTop was removed from Footer

--- src/components/Footer/Footer.styles.ts
+++ src/components/Footer/Footer.styles.ts
@@ -4,6 +4,5 @@ export const FOOTER_STYLES: SxProps<Theme> = {
   backgroundColor: '#F0F0F2',
   color: 'secondary.main',
   textAlign: 'center',
-  mt: (theme) => theme.spacing(5),
   p: (theme) => theme.spacing(1.25),
 };
